32475efb714b1dc4271741494dec5c9e33b6e217
Jonathan Danse <j.danse@prestaedit.com>
2024-02-22 10:37:23 +0100

    Add nunzion/php-expect

---
+++ src/Expects/Expect.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PrestaFlow\Library\Expect;
+
+use Nunzion\Expect as ExpectLibrary;
+
+class Expect extends ExpectLibrary
+{
+
+}
--- src/Tests/Suites/Login/LoginTest.php
+++ src/Tests/Suites/Login/LoginTest.php
@@ -2,16 +2,31 @@
 
 namespace PrestaFlow\Library\Tests\Suites\Login;
 
+use PrestaFlow\Library\Excepts\Except;
 use PrestaFlow\Library\Tests\TestsSuite;
 
 class LoginTest extends TestsSuite
 {
     public function __construct()
     {
+        $page = $this->page;
         $this->describe(
             'Check PS version ${global.PS_VERSION} with ${global.LOCALE} language, and login and log out from BO',
             [
                 $this->it('should go to login page', function () use ($page) {
+                    Expect::that("foo")->equals("foo"); //"foo" == "foo"
+
+                }),
+                $this->it('should check PS version', function () use ($page) {
+
+                }),
+                $this->it('should try to login with wrong email and password', function () use ($page) {
+
+                }),
+                $this->it('should login into BO with default user', function () use ($page) {
+
+                }),
+                $this->it('should log out from BO', function () use ($page) {
 
                 }),
             ]
@@ -19,10 +34,34 @@ class LoginTest extends TestsSuite
     }
 
     /*
-  it('should go to login page', async () => {
-    await loginPage.goTo(page, global.BO.URL);
-    const pageTitle = await loginPage.getPageTitle(page);
-    await expect(pageTitle).to.contains(loginPage.pageTitle);
-  });
-   */
+    it('should go to login page', async () => {
+        await loginPage.goTo(page, global.BO.URL);
+        const pageTitle = await loginPage.getPageTitle(page);
+        await expect(pageTitle).to.contains(loginPage.pageTitle);
+    });
+    it('should check PS version', async () => {
+        const psVersion = await loginPage.getPrestashopVersion(page);
+        await expect(psVersion).to.contains(global.PS_VERSION);
+    });
+    it('should try to login with wrong email and password', async () => {
+        await loginPage.login(page, 'wrongEmail@prestashop.com', 'wrongPass', false);
+
+        // Get error displayed
+        const errorMessage = await loginPage.getLoginError(page);
+        await expect(errorMessage).to.contain(loginPage.loginErrorText);
+    });
+    it('should login into BO with default user', async () => {
+        await loginPage.login(page, global.BO.EMAIL, global.BO.PASSWD);
+        await dashboardPage.closeOnboardingModal(page);
+
+        const pageTitle = await dashboardPage.getPageTitle(page);
+        await expect(pageTitle).to.contains(dashboardPage.pageTitle);
+    });
+    it('should log out from BO', async () => {
+        await dashboardPage.logoutBO(page);
+
+        const pageTitle = await loginPage.getPageTitle(page);
+        await expect(pageTitle).to.contains(loginPage.pageTitle);
+    });
+    */
 }
