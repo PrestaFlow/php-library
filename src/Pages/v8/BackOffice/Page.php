fd910c178c1f0578b3d2dff92451d3d68a63a317
Jonathan Danse <j.danse@prestaedit.com>
2025-07-14 13:44:46 +0200

    feat: use common pages

--- src/Pages/v8/BackOffice/Page.php
+++ src/Pages/v8/BackOffice/Page.php
@@ -2,57 +2,8 @@
 
 namespace PrestaFlow\Library\Pages\v8\BackOffice;
 
-use PrestaFlow\Library\Pages\CommonPage;
+use PrestaFlow\Library\Pages\BackOfficePage;
 
-class Page extends CommonPage
+class Page extends BackOfficePage
 {
-    public function __construct(string $locale, string $patchVersion, array $globals)
-    {
-        $this->globals = $globals;
-
-        $selectors = [
-        ];
-
-        $pageSelectors = [];
-        if (method_exists($this, 'defineSelectors')) {
-            $pageSelectors = $this->defineSelectors();
-        }
-
-        $this->selectors = [...$selectors, ...$pageSelectors];
-
-        $messages = [];
-
-        $pageMessages = [];
-        if (method_exists($this, 'defineMessages')) {
-            $pageMessages = $this->defineMessages();
-        }
-
-        $this->messages = [...$messages, ...$pageMessages];
-
-        parent::__construct(locale: $locale, patchVersion: $patchVersion, globals: $globals);
-    }
-
-    public function goToPage($page = null)
-    {
-        if ($page === null) {
-            $page = 'login';
-        }
-
-        $url = $this->getPageURL($page);
-        $this->getPage()->navigate($url)->waitForNavigation();
-    }
-
-    public function getPageURL($page) : string
-    {
-        $url = $this->getGlobals()['BO']['URL'];
-        if (is_string($page)) {
-            $url .= match ($page) {
-                'login', 'index' => '',
-            };
-        } else if (is_object($page)) {
-            $url .= '/'.$page->url;
-        }
-
-        return $url;
-    }
 }
